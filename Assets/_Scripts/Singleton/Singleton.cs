63edf3d1668c63c37d40063c8b84ddf6533f0eb5
HangLiangYuan <348449959@qq.com>
2025-04-29 21:02:44 -0700

    更新了JsonService和ServiceProvider
    增加了单例基类，设置了是否DontDestroyOnLoad的boolean判断。创建了ServiceProvider，利用Type和继承自IService接口的服务类接口字典注册查找服务。JsonService实现了从streamingasset路径和数据持久化路径读取数据和存储数据。添加了供测试的JsonServiceTest脚本。

---
+++ Assets/_Scripts/Singleton/Singleton.cs
@@ -0,0 +1,60 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace Demo.Core
+{
+    public class Singleton<T> : SerializedMonoBehaviour where T : Singleton<T>
+    {
+        #region 单例变量与属性
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindFirstObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        Debug.LogError($"[Singleton] 场景中找不到 {typeof(T).Name} 实例，请确保场景中存在或手动创建。");
+                    }
+                }
+                return _instance;
+            }
+        }
+        #endregion
+        
+        [OdinSerialize] private bool _isDontDestroyOnLoad = true;
+
+        protected virtual void Initialize()
+        {
+            
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+
+                if (_isDontDestroyOnLoad)
+                {
+                    DontDestroyOnLoad(gameObject);
+                    Debug.Log($"[SingletonBase] {typeof(T).Name} 启用 DontDestroyOnLoad。");
+                }
+                
+                Initialize();
+            }
+            else
+            {
+                if (_instance != this)
+                {
+                    Debug.LogWarning($"[Singleton] 检测到重复单例 {typeof(T).Name}，自动销毁后来的实例。");
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
